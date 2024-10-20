ddffcd81f80c53bbd78a3676c41b896394079348
Maciej Malinowski <mejsiejdev@gmail.com>
2024-10-20 10:38:13 +0200

    Add Rubik font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,21 @@
 import type { Config } from "tailwindcss";
-
+const defaultTheme = require('tailwindcss/defaultTheme')
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    fontFamily: {
+      sans: ['Rubik', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      fontFamily: {
+        icons: ["Material Icons", "sans-serif"]
+      }
+    }
+  },
   plugins: [],
 };
 export default config;
