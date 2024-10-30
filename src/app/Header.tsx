3a19654965a5a79520f3128b4bbcdb430e09ef88
Maciej Malinowski <mejsiejdev@gmail.com>
2024-10-30 18:09:09 +0100

    Update `header`

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 export default function Header() {
   const [input, setInput] = useState("");
   const { filters, setFilters } = useContext(FiltersContext);
-  const { setShowFilters } = useContext(ShowFiltersContext);
+  const { showFilters, setShowFilters } = useContext(ShowFiltersContext);
   // Debounce setting query
   useEffect(() => {
     const timeout = setTimeout(() => {
@@ -25,17 +25,17 @@ export default function Header() {
       <header className="w-full flex flex-row gap-4 justify-between items-center p-4">
         <div className="flex flex-row gap-2 items-center">
           <p className="font-icons text-3xl">liquor</p>
-          <h1 className="text-2xl font-semibold">Cocktails</h1>
+          <h1 className="hidden sm:block text-2xl font-semibold">Cocktails</h1>
         </div>
         <input
           onChange={(e) => setInput(e.target.value)}
           placeholder="Search"
-          className="text-lg bg-neutral-100 dark:bg-neutral-700 w-full rounded-full h-full px-4 py-1 text-sm focus:ring-2 focus:ring-neutral-300 border-0 focus:border-1 border-neutral-300"
+          className="bg-neutral-100 dark:bg-neutral-700 w-full rounded-full h-full px-4 py-1 focus:ring-2 focus:ring-neutral-300 border-0 focus:border-1 border-neutral-300"
           type="text"
         />
         <button
-          title="Filtruj cocktaile"
-          onClick={() => setShowFilters(true)}
+          title="Filter cocktails"
+          onClick={() => setShowFilters(!showFilters)}
           className="font-icons text-3xl flex-none flex"
         >
           tune
